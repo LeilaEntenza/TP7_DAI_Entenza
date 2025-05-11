dd3c24db0dbcea289437a7e2b0836fff1f29a006
Lei <leilachiderski@gmail.com>
2025-05-11 17:23:01 -0300

    Error arreglado y foto agregada

--- tpdai7/App.js
+++ tpdai7/App.js
@@ -1,15 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { ImageBackground, SafeAreaView, StyleSheet, Text, View } from 'react-native'; 
+import { ImageBackground, SafeAreaView, StyleSheet, Text, View, Image } from 'react-native'; 
 
 export default function App() {
   const imgExterna = {uri: 'https://i.pinimg.com/736x/58/45/33/584533963b52ef77f22f2e9d4c276c90.jpg'};
   return (
     <ImageBackground 
       source={imgExterna}
+      style={{flex:1}}
       resizeMethod= "cover"
       >
       <SafeAreaView style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Image source={{uri: 'https://i.pinimg.com/736x/7d/a8/d0/7da8d04ef76e488a0b6117b97efece9d.jpg'}} style={styles.FotoPerfil}/>
         <StatusBar style="auto" />
       </SafeAreaView>
     </ImageBackground>
@@ -21,5 +22,11 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  FotoPerfil: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
   },
 });
